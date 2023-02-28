9577a832fb1ca98cdd4658008d863b582218c1f0
Aster Haven (@therebelrobot) <asterhaven@pm.me>
2023-02-28 11:26:23 -0500

    removed overriding metadata

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,10 +1,6 @@
 import type { GatsbyConfig } from "gatsby";
 
 const config: GatsbyConfig = {
-  siteMetadata: {
-    title: `Coven of the Woods`,
-    siteUrl: `https://covenofthewoods.church`,
-  },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
